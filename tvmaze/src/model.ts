1b2a9a691dc1165f6a4fe4856781c350b4707d78
Crystal Tran <c.t.tran95@gmail.com>
2024-01-29 15:32:10 -0500

    completed searchShowsByTerm name

--- tvmaze/src/model.ts
+++ tvmaze/src/model.ts
@@ -1,6 +1,22 @@
 const MISSING_IMAGE_URL = "https://tinyurl.com/missing-tv";
 const TVMAZE_API_URL = "https://api.tvmaze.com/";
 
+interface IShowsReturns {
+  id: number,
+  name: string,
+  summary: string,
+  image: string
+}
+
+interface IShows {
+  show: {
+    id: number,
+    name: string,
+    summary: string,
+    image: { medium: string; };
+  };
+}
+
 /** Given a search term, search for tv shows that match that query.
  *
  *  Returns (promise) array of show objects: [show, show, ...].
@@ -8,26 +24,21 @@ const TVMAZE_API_URL = "https://api.tvmaze.com/";
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function searchShowsByTerm(term) {
+async function searchShowsByTerm(term: string): Promise <IShowsReturns[]> {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-        "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  const showSearchParams = new URLSearchParams({ q: term });
+  const response = await fetch(`${TVMAZE_API_URL}search/shows${showSearchParams}`);
+  const apiShowData = await response.json();
+
+  return apiShowData.map((s: IShows) => {
+    const result = s.show;
+    return {
+      id: result.id,
+      name: result.name,
+      summary: result.summary,
+      image: result.image.medium as string || MISSING_IMAGE_URL;
+    };
+  });
 }
 
 
